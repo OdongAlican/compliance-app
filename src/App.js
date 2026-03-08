79d05e78078168adc369e1f5d5bc07e930df45c8
Odong Sunday <sandieo.2020@gmail.com>
2026-03-08 21:56:14 +0300

    add redux to app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import './index.css';
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import { ThemeProvider } from './context/ThemeContext';
+import { AuthProvider } from './context/AuthContext';
+import store from './store';
 import AppRoutes from './routes';
 
 
@@ -12,15 +15,19 @@ import AppRoutes from './routes';
 
 export default function App() {
   return (
+    <Provider store={store}>
     <ThemeProvider>
-      <Router>
-        <div
-          className="min-h-screen"
-          style={{ background: 'var(--bg)', color: 'var(--text)', transition: 'background .3s, color .3s' }}
-        >
-          <AppRoutes />
-        </div>
-      </Router>
+      <AuthProvider>
+        <Router>
+          <div
+            className="min-h-screen"
+            style={{ background: 'var(--bg)', color: 'var(--text)', transition: 'background .3s, color .3s' }}
+          >
+            <AppRoutes />
+          </div>
+        </Router>
+      </AuthProvider>
     </ThemeProvider>
+    </Provider>
   );
 }
